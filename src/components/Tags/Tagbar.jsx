8e9ebf4a36e6a380008f8b233a277924edd3da52
Abhishek122504 <abhiabhi200412@gmail.com>
2025-07-25 15:43:02 +0530

    empty component

--- src/components/Tags/Tagbar.jsx
+++ src/components/Tags/Tagbar.jsx
@@ -5,7 +5,7 @@ import Tag from './Tag'
 const Tagbar = () => {
     return (
         <div className='max-w-full overflow-hidden'>
-            <div className='flex items-center overflow-x-scroll gap-3 scrollbar-hide bg-gray-200 py-3 px-2 mt-2 whitespace-nowrap'>
+            <div className='flex items-center overflow-x-scroll gap-5 scrollbar-hide bg-gray-200 py-3 px-2 mt-2 whitespace-nowrap'>
                 {
                     tags.length > 0 ?
                         tags.map((tag) => (
--- src/components/Tags/tagData.js
+++ src/components/Tags/tagData.js
@@ -22,7 +22,43 @@ const tags = [
     {
         id : 6,
         desc : 'C++'
-    }
+    },
+    {
+        id : 7,
+        desc : 'Java'
+    },
+    {
+        id : 8,
+        desc : 'GO'
+    },
+    {
+        id : 9,
+        desc : 'Javascript'
+    },
+    {
+        id : 10,
+        desc : 'React JS'
+    },
+    {
+        id : 11,
+        desc : 'Visual Studio Code'
+    },
+    {
+        id : 12,
+        desc : 'MSIT'
+    },
+    {
+        id : 13,
+        desc : 'Example.com'
+    },
+    {
+        id : 14,
+        desc : 'Question Tags'
+    },
+    {
+        id : 15,
+        desc : 'Replied'
+    },
 ];
 
 export default tags;
