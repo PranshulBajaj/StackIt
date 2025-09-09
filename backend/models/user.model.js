0d160b66519bad3fe3da726b22ac61c264f3844e
Abhishek122504 <abhiabhi200412@gmail.com>
2025-09-09 22:26:25 +0530

    user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,2 +1,34 @@
 import mongoose,{Schema} from "mongoose";
-import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    avatar: {
+        type: String, 
+        required: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken: {
+        type: String
+    }
+}, {
+    timestamps: true
+})
+
+export const User = mongoose.model('User', userSchema);
